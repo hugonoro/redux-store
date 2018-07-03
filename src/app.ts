9d4ae44db768947830a731ccda94187bf7b80d6d
Hugo Noro <hnoro2@sapient.com>
2018-07-03 14:16:29 +0100

    Dispatching actions

--- src/app.ts
+++ src/app.ts
@@ -8,29 +8,32 @@ const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
 
 const store = new fromStore.Store(
-  {},
-  {
-    todos: [{ label: 'Eat pizza', complete: false }]
-  }
+    {},
+    {
+        todos: [{ label: 'Eat pizza', complete: false }]
+    }
 );
 
 button.addEventListener(
-  'click',
-  () => {
-    if (!input.value.trim()) return;
+    'click',
+    () => {
+        if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
+        const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+        store.dispatch({
+            type: 'ADD_TODO',
+            payload
+        });
 
-    input.value = '';
-  },
-  false
+        input.value = '';
+    },
+    false
 );
 
 todoList.addEventListener('click', function (event) {
-  const target = event.target as HTMLButtonElement;
-  if (target.nodeName.toLowerCase() === 'button') {
-    console.log(target);
-  }
+    const target = event.target as HTMLButtonElement;
+    if (target.nodeName.toLowerCase() === 'button') {
+        console.log(target);
+    }
 });
