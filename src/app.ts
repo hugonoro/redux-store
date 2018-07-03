e74b2d43540e5d5de5e4d2a5053a10d3b54e5f1b
Hugo Noro <hnoro2@sapient.com>
2018-07-03 12:28:07 +0100

    Store creation and initial state

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
+import * as fromStore from './store';
+
 import { renderTodos } from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const store = new fromStore.Store(
+  {},
+  {
+    todos: [{ label: 'Eat pizza', complete: false }]
+  }
+);
 
 button.addEventListener(
   'click',
@@ -19,7 +28,7 @@ button.addEventListener(
   false
 );
 
-todoList.addEventListener('click', function(event) {
+todoList.addEventListener('click', function (event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
     console.log(target);
