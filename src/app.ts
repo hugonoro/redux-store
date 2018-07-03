bd9fea3b2ca2b339b9fbf0b475f079172723dd8c
Hugo Noro <hnoro2@sapient.com>
2018-07-03 16:24:18 +0100

    Action creators

--- src/app.ts
+++ src/app.ts
@@ -21,10 +21,7 @@ button.addEventListener(
 
         const payload = { label: input.value, complete: false };
 
-        store.dispatch({
-            type: 'ADD_TODO',
-            payload
-        });
+        store.dispatch(new fromStore.AddTodo(payload));
 
         input.value = '';
     },
@@ -40,6 +37,7 @@ destroy.addEventListener('click', unsubscribe, false);
 todoList.addEventListener('click', function (event) {
     const target = event.target as HTMLButtonElement;
     if (target.nodeName.toLowerCase() === 'button') {
-        console.log(target);
+        const todo = JSON.parse(target.getAttribute('data-todo') as any);
+        store.dispatch(new fromStore.RemoveTodo(todo))
     }
 });
